8a593fc391172b87bb410d560953161f56970fb8
Li <CLI002@ets.org>
2019-08-11 21:57:44 -0400

    add child component female and male component under employee list component and setting child route navigation

--- src/app/employee-list/employee-list.module.ts
+++ src/app/employee-list/employee-list.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { EmployeeListComponent } from './employee-list.component';
 import { RouterModule } from '@angular/router';
 import { EMPLOYEE_ROUTES } from './employee.routes';
+import { MaleEmployeeComponent } from './male-employee/male-employee.component';
+import { FemaleEmployeeComponent } from './female-employee/female-employee.component';
 
 
 
 @NgModule({
   declarations: [
-    EmployeeListComponent
+    EmployeeListComponent,
+    MaleEmployeeComponent,
+    FemaleEmployeeComponent
   ],
   imports: [
     CommonModule,
--- src/app/employee-list/employee.routes.ts
+++ src/app/employee-list/employee.routes.ts
@@ -1,8 +1,21 @@
 import { EmployeeListComponent } from './employee-list.component';
+import { MaleEmployeeComponent } from './male-employee/male-employee.component';
+import { FemaleEmployeeComponent } from './female-employee/female-employee.component';
 
 export const EMPLOYEE_ROUTES = [
     {
         path: 'employee',
-        component: EmployeeListComponent
+        component: EmployeeListComponent,
+        children: [
+            {
+                path: 'male',
+                component: MaleEmployeeComponent
+            },
+            {
+                path: 'female',
+                component: FemaleEmployeeComponent
+            }
+        ]
+
     }
-]
+];
